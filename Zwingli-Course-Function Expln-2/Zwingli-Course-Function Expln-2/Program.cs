bba2a29e4b874e4897f5379ccf3bff4146eab396
joshtri <stuffofyos1516@gmail.com>
2023-02-18 07:21:19 +0800

    new-prog

---
+++ Zwingli-Course-Function Expln-2/Zwingli-Course-Function Expln-2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zwingli_Course_Function_Expln_2
+{
+    internal class Program
+    {
+
+        /// <catatan>
+        ///  
+        /// 
+        /// 
+        /// 
+        /// 
+        ///  I. Method diketahui memiliki 2 macam jenis : 
+        /// 
+        ///      - static void
+        ///        Pada method ini tidak memerlukan return value.
+        /// 
+        ///      - static non-void
+        ///         Pada method ini memerlukan return value(nilai balik).
+        /// 
+        /// 
+        /// 
+        ///         "METHOD DIAWALI DENGAN STATIC"
+        /// </catatan>
+        /// 
+        /// 
+        /// 
+        /// <param name="args"></param>
+        static void Main(string[] args) //Main program. / Global.
+        {
+
+        }
+
+
+
+        //syntax method. 
+        //<Access Specifier> <Return Type> <Method Name>(Parameter List) 
+        //{ 	
+        //    Access Body
+        //}
+
+        //non void. 
+        public static int HitungLuas (int sisi) 
+        {
+
+            //access body.
+            return 0; 
+            
+        }
+        
+        
+
+        
+    }
+}
---
+++ Zwingli-Course-array1d/Zwingli-Course-array1d/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zwingli_Course_array1d
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //data array statis 
+            //int[] ageStudent = new int[4] { 50, 10, 20, 10 };
+            //ageStudent[4] = 0;
+
+
+            //data array dinamis
+
+            Console.Write("Input ukuran array : ");
+            int arraySize = int.Parse(Console.ReadLine());
+
+            string[] cars = new string[arraySize]; //1D
+            Console.WriteLine();
+
+
+            InputArray(cars);
+
+            Console.WriteLine("Cetak data mobil tahun 2023 : \n\n");
+            //cetak.
+
+            //tidak perlu deklarasi. panggil variabel saja. 
+            CetakData(cars);
+ 
+        }
+
+        public static void CetakData(string [] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void InputArray(string[] array)
+        {
+            for (int index = 0; index < array.Length; index++)
+            {
+                Console.Write($"input data ke-{index} = ");
+                array[index] = Console.ReadLine();
+            }
+        }
+
+        public static void CetakDataint(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void InputArrayint(int[] array)
+        {
+            for (int index = 0; index < array.Length; index++)
+            {
+                Console.Write($"input data ke-{index} = ");
+                array[index] = int.Parse(Console.ReadLine());
+            }
+        }
+
+    }
+}
